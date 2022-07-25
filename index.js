c2d0971d176b2d33ae6558a7a3ee94cbe5acd442
ekowbossman <ekowbossman@gmail.com>
2022-07-24 22:24:09 -0400

    made changes to deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require("uuid");
 var notesDB = require("./db/db.json");
 
 const app = express();
-const PORT = process.env.PORT || 3000 ; 
+const PORT = process.env.PORT || 3001; 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
